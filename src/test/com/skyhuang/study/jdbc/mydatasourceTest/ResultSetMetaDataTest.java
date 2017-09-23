e921a79a0c57d8382b195f0f470ee563ce0abc52
hkpp6548 <510373198@qq.com>
2017-09-23 23:01:50 +0800

    数据库元数据 2

--- src/test/com/skyhuang/study/jdbc/mydatasourceTest/ResultSetMetaDataTest.java
+++ src/test/com/skyhuang/study/jdbc/mydatasourceTest/ResultSetMetaDataTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 import java.sql.*;
 
-/**
+/**`结果集元数据
  * Created by dahoufang the one on 2017/9/23.
  */
 public class ResultSetMetaDataTest {
@@ -15,17 +15,22 @@ public class ResultSetMetaDataTest {
 		try {
 			Connection connection = cpds.getConnection();
 			String sql = "select * from account";
-
-
 			PreparedStatement preparedStatement = connection.prepareStatement(sql);
-			//preparedStatement.setString(1,"in");
-			ParameterMetaData metaData = preparedStatement.getParameterMetaData();
-
-			ResultSet resultSet = preparedStatement.getResultSet();
-
-
-
-
+			ResultSet resultSet= preparedStatement.executeQuery(sql);
+			ResultSetMetaData metaData = resultSet.getMetaData();
+			int columnCount = metaData.getColumnCount();//获取结果集中列数量
+			for (int i = 1; i <= columnCount; i++) {
+				String columnName = metaData.getColumnName(i);//获取结果集中指定列的名称.
+				String columnTypeName = metaData.getColumnTypeName(i);
+				System.out.print(columnName + "("+columnTypeName+")" + "\t");
+			}
+			System.out.println();
+			while (resultSet.next()){
+				String id = resultSet.getString("id");
+				String account = resultSet.getString("account");
+				double money = resultSet.getDouble("money");
+				System.out.println(account + "\t\t\t\t\t" + money + "\t\t\t\t" + id + "\t");
+			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
