074786f577f569e7bb6de810f31e3f6112e23dd5
hkpp6548 <510373198@qq.com>
2017-10-18 19:16:59 +0800

    上传下载3

--- src/main/java/com/skyhuang/study/program/upDownload/controller/ShowDownloadServlet.java
+++ src/main/java/com/skyhuang/study/program/upDownload/controller/ShowDownloadServlet.java
@@ -1,20 +1,31 @@
 package com.skyhuang.study.program.upDownload.controller;
 
+import com.skyhuang.study.program.upDownload.domain.Resources;
+import com.skyhuang.study.program.upDownload.service.UpDownloadService;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
 
-/**
+/** 展示需要下载的文件控制器
  * Created by hk on 2017/10/17.
  */
-@WebServlet(name = "ShowDownloadServlet")
+@WebServlet(name = "ShowDownloadServlet",urlPatterns = "/showDownloadServlet")
 public class ShowDownloadServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        UpDownloadService service = new UpDownloadService();
+        try {
+            List<Resources> resources = service.selectAll();
 
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
