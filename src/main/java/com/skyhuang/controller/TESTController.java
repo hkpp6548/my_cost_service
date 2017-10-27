3a86b8d5eac0202e62ea43ac5a72e1ad4a39e97d
hkpp6548 <510373198@qq.com>
2017-10-27 17:43:25 +0800

    estore项目开始 删除多余的 重新开始

--- src/main/java/com/skyhuang/controller/TESTController.java
+++
@@ -1,7 +0,0 @@
-package com.skyhuang.controller;
-
-/** 测试
- * Created by skyhuang on 2017/8/29.
- */
-public class TESTController {
-}
--- src/main/java/com/skyhuang/study/designMode/proxy/privileges/dao/BookPrivilegesDaoImpl.java
+++
@@ -1,8 +0,0 @@
-package com.skyhuang.study.designMode.proxy.privileges.dao;
-
-/**
- * Created by dahoufang the one on 2017/10/26.
- */
-public class BookPrivilegesDaoImpl {
-
-}
