870ee8ad48342187e4e837b7bb6f07e11513750a
hkpp6548 <510373198@qq.com>
2017-11-16 09:53:10 +0800

    新建分支，删除之前内容。

--- src/main/java/com/skyhuang/study/designMode/proxy/KingWoman.java
+++
@@ -1,9 +0,0 @@
-package com.skyhuang.study.designMode.proxy;
-
-/** 风流的女人接口
- * Created by dahoufang the one on 2017/10/25.
- */
-public interface KingWoman {
-    public void throwEye();
-    public void doSomething();
-}
